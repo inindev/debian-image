d1fe7cb8e751b2a170fafdd09d6cecdb3fd4f0dd
John Clark <inindev@gmail.com>
2024-01-13 17:43:13 +0000

    u-boot binaries downloader
    Signed-off-by: John Clark <inindev@gmail.com>

---
+++ scripts/get_uboot.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Copyright (C) 2024, John Clark <inindev@gmail.com>
+
+set -e
+
+
+main() {
+    rm -rf 'downloads/uboot'
+    mkdir -p 'downloads/uboot'
+
+    local tmp="$(mktemp)"
+    wget -O "$tmp" 'https://github.com/inindev/uboot-rockchip/releases/latest/download/sha256sums.txt'
+
+    local line file sha
+    while read line; do
+        file=${line#*  }
+        sha=${line%  *}
+        get_file "$file" "$sha"
+    done < "$tmp"
+
+    rm -f "$tmp"
+}
+
+get_file() {
+    local file="$1"
+    local sha="$2"
+
+    wget -P 'downloads/uboot' "https://github.com/inindev/uboot-rockchip/releases/latest/download/$file"
+    [ "$sha" = $(sha256sum "downloads/uboot/$file" | cut -c1-64) ] || { echo "invalid hash for downloads/uboot/$file"; exit 5; }
+}
+
+cd "$(dirname "$(realpath "$0")")/.."
+main "$@"
