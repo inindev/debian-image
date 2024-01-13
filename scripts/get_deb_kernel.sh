c067c3bc63296c0d0b6fb6417f938f9704209a4b
John Clark <inindev@gmail.com>
2024-01-13 17:42:18 +0000

    switch from curl to wget
    Signed-off-by: John Clark <inindev@gmail.com>

--- scripts/get_deb_kernel.sh
+++ scripts/get_deb_kernel.sh
@@ -8,13 +8,15 @@ set -e
 main() {
     local dist="${1:-stable}"
 
-    local kpath="$(curl -s "https://packages.debian.org/$dist/kernel/linux-image-arm64" | grep -A1 'dep:' | sed -rn 's|.*<a href=\"(.*)\">.*|\1|p')"
+    echo "fetching: https://packages.debian.org/$dist/kernel/linux-image-arm64"
+    local kpath="$(wget -qO - "https://packages.debian.org/$dist/kernel/linux-image-arm64" | grep -A1 'dep:' | sed -rn 's|.*<a href=\"(.*)\">.*|\1|p')"
 
     local kdir="$(dirname $kpath)"
     local kfile="$(basename $kpath)"
 
+    echo "fetching: https://packages.debian.org${kdir}/arm64/${kfile}/download"
     local kpre="https://packages.debian.org${kdir}/arm64/${kfile}/download"
-    local kurl="$(curl -s $kpre | sed -rn 's|.*href=\"(.*_arm64.deb)\".*|\1|p' | head -n1)"
+    local kurl="$(wget -qO - $kpre | sed -rn 's|.*href=\"(.*_arm64.deb)\".*|\1|p' | head -n1)"
 
     echo "fetching: $kurl"
     wget -P "downloads" "$kurl"
